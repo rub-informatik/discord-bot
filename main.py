58cff7325a04e06331354cdcecf6e73160b51a8f
Jartreg <Jartreg@users.noreply.github.com>
2020-10-06 19:02:49 +0200

    Create templates for bot components (#2)

--- main.py
+++ main.py
@@ -5,9 +5,22 @@ import logging
 from discordbot import DiscordBot, DiscordClient
 from discordbot.config import from_env
 
+from discordbot.notifications import NotificationsComponent
+from discordbot.roles import RolesComponent
+
+
+def create_components(bot: DiscordBot):
+    """Configure and add components to the bot"""
+
+    bot.add_component(NotificationsComponent, "notifications")
+    bot.add_component(RolesComponent, "roles")
+
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
     client = DiscordClient(from_env("DISCORD_TOKEN"))
     bot = DiscordBot(client)
+    create_components(bot)
+
     bot.run()
